6f2f8c662a1c585e87b4735042a3c9d4482a53a1
Laika-Beats <laikabeatsdenver@gmail.com>
2021-01-16 20:32:38 -0700

    new entry schema + post route

--- routes/entries.js
+++ routes/entries.js
@@ -1,8 +1,28 @@
 const express = require("express");
+const Entry = require("../models/entry");
 const router = express.Router();
 
 router.get("/new", (req, res) => {
-  res.render("entries/new");
+  res.render("entries/new", { entry: new Entry() });
+});
+
+// router.get("/:id" (req, res) => {
+
+// })
+
+router.post("/", async (req, res) => {
+  let entry = new Entry({
+    title: req.body.title,
+    description: req.body.description,
+    markdown: req.body.markdown,
+  });
+  try {
+    entry = await entry.save();
+    res.redirect(`/entries/${entry.id}`);
+  } catch (error) {
+    console.log(error);
+    res.render("entries/new", { entry: entry });
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const entryRouter = require("./routes/entries.js");
 const app = express();
 
+mongoose.connect("mongodb://localhost/journal", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: false }));
 app.use("/entries", entryRouter);
 
 app.get("/", (req, res) => {
